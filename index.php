3960c55e911ea4d8f0eb8e0f480441e23915d423
Ian Greene <ian@donorsearch.net>
2019-09-09 15:49:09 -0400

    added pricing stuff and seperare sections for content, added some placeholder photos

--- index.php
+++ index.php
@@ -4,9 +4,108 @@
      */
     include_once './header.html';
     ?>
+<!-- Start of Content -->
+<!-- Heading -->
+<section id="heading" class="text-center">
+    <div class="row 5 my-4">
+        <div class="col-md-4 offset-2">
+            <img src="./images/desktop.png" alt="desktop-photo">
+        </div>
+        <div class="col-md-4 py-5">
+            <h1 class="display-4">Know Your Prospects</h1>
+            <p>
+                Net worth and Lead Qualification
+            
+                No need to speak to a sales person
+                Jump in and try our tool for one week free
+                Browse our clear pricing starting at $19.99
+            </p>
+        </div>
+    </div>
+</section>
+<!-- End Heading -->
 
+<!-- What We Do -->
+<section id="what-we-do" class="text-center bg-086375 py-4">
+    <div class="row">
+        <div class="col-md-4 offset-2">
+            <h1 class="display-4">What We Do</h1>
+            <p class="mt-4 lead">We help identify your top prospects. Lets be honest you need money, and they have it. The problem lies in who to ask and how much to ask for.</p>
+        </div>
+        <div class="col-md-4">
+            <img class="what-we-do-img" src="./images/moneysearch.jpg" alt="moneysearch">
+        </div>
+    </div>
+</section>
+<!-- End What We Do -->
 
+<!-- Pricing -->
+<section id="pricing">
+    <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+        <h1 class="display-4">Pricing</h1>
+        <p class="lead">Quickly build an effective pricing table for your potential customers with this Bootstrap example.
+            It's built with default Bootstrap components and utilities with little customization.</p>
+    </div>
+    <div class="container">
+        <div class="row mb-3 text-center">
+            <div class="col-md-4">
+                <div class="card mb-4 box-shadow">
+                    <div class="card-header bg-33658a">
+                        <h4 class="my-0 font-weight-normal">Free</h4>
+                    </div>
+                    <div class="card-body">
+                        <h1 class="card-title pricing-card-title">$0 <small class="text-muted">/ mo</small></h1>
+                        <ul class="list-unstyled mt-3 mb-4">
+                            <li>10 users included</li>
+                            <li>2 GB of storage</li>
+                            <li>Email support</li>
+                            <li>Help center access</li>
+                        </ul>
+                        <button type="button" class="btn btn-lg btn-block btn-outline-ds">Sign up for free</button>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card mb-4 box-shadow">
+                    <div class="card-header bg-4899ac">
+                        <h4 class="my-0 font-weight-normal">Pro</h4>
+                    </div>
+                    <div class="card-body">
+                        <h1 class="card-title pricing-card-title">$15 <small class="text-muted">/ mo</small></h1>
+                        <ul class="list-unstyled mt-3 mb-4">
+                            <li>20 users included</li>
+                            <li>10 GB of storage</li>
+                            <li>Priority email support</li>
+                            <li>Help center access</li>
+                        </ul>
+                        <button type="button" class="btn btn-lg btn-block btn-ds">Get started</button>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card mb-4 box-shadow">
+                    <div class="card-header bg-55dde0">
+                        <h4 class="my-0 font-weight-normal">Enterprise</h4>
+                    </div>
+                    <div class="card-body">
+                        <h1 class="card-title pricing-card-title">$29 <small class="text-muted">/ mo</small></h1>
+                        <ul class="list-unstyled mt-3 mb-4">
+                            <li>30 users included</li>
+                            <li>15 GB of storage</li>
+                            <li>Phone and email support</li>
+                            <li>Help center access</li>
+                        </ul>
+                        <button type="button" class="btn btn-lg btn-block btn-ds">Contact us</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 
+<!-- End Pricing -->
+
+<!-- End of Content -->
 <?php
     /**
      *  Import Footer
